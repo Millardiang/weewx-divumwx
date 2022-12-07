065b9a5d9438f508ce489a8f5d507f5901cb094a
Millardiang <78812410+Millardiang@users.noreply.github.com>
2022-12-07 11:26:26 +0000

    Update dvmGetMoon.php

--- www/dvmGetMoon.php
+++ www/dvmGetMoon.php
@@ -1,9 +1,5 @@
 <?php
 include("dvmCombinedData.php");
-if ($lat >= "0" && $lat <= "180"){$NS = "North";}
-else {$NS = "South";}
-if ($lon >= "0" && $lat <= "180"){$EW = "East";}
-else {$EW = "West";}
 $url_to_image = "https://www.fourmilab.ch/cgi-bin/Earth?img=LRO_100m.evif&imgsize=320&dynimg=y&gamma=1.32&opt=-l&lat=".$lat."&ns=".$NS."&lon=".$lon."&ew=".$EW."&alt=35785&tle=&date=0&utc=&jd=/Earth.jpg";$my_save_dir = '/var/www/html/weewx/divumwx/img/';
 $filename = 'moon-1.jpg';
 $complete_save_loc = $my_save_dir . $filename;
