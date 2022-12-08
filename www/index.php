fb8744b7eaedb3c54aceb3f036ac83bd4e58afe9
Millardiang <78812410+Millardiang@users.noreply.github.com>
2022-12-08 08:03:58 +0000

    Update index.php

--- www/index.php
+++ www/index.php
@@ -66,7 +66,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 </script> 
-<script src="https://cdnres.willyweather.co.uk/widget/warning/loadView.html?id=3837" type="application/javascript"></script>
+
 </head>
 
 <!-- begin top layout-->
