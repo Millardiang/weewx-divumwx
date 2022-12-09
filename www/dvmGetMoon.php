478d27e34b4585cb443ddf707980b56830e3fecf
Millardiang <78812410+Millardiang@users.noreply.github.com>
2022-12-09 11:45:52 +0000

    Simplified path

--- www/dvmGetMoon.php
+++ www/dvmGetMoon.php
@@ -1,7 +1,6 @@
 <?php
 include("dvmCombinedData.php");
-$url_to_image = "https://www.fourmilab.ch/cgi-bin/Earth?img=LRO_100m.evif&imgsize=320&dynimg=y&gamma=1.32&opt=-l&lat=".$lat."&ns=".$NS."&lon=".$lon."&ew=".$EW."&alt=35785&tle=&date=0&utc=&jd=/Earth.jpg";$my_save_dir = '/var/www/html/weewx/divumwx/img/';
-$filename = 'moon-1.jpg';
-$complete_save_loc = $my_save_dir . $filename;
-file_put_contents($complete_save_loc, file_get_contents($url_to_image));
+$url_to_image = "https://www.fourmilab.ch/cgi-bin/Earth?img=LRO_100m.evif&imgsize=320&dynimg=y&gamma=1.32&opt=-l&lat=".$lat."&ns=".$NS."&lon=".$lon."&ew=".$EW."&alt=35785&tle=&date=0&utc=&jd=/Earth.jpg";
+$filename = 'img/moon-1.jpg';
+file_put_contents($filename, file_get_contents($url_to_image));
 ?>
