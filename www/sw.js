472385b4a9d64863b1f9b6b4ce60b09b9e20dbd4
Millardiang <78812410+Millardiang@users.noreply.github.com>
2023-02-10 11:31:37 +0000

    Updated sw.js

--- www/sw.js
+++ www/sw.js
@@ -1,12 +1,44 @@
-self.addEventListener('install', event => {
-  console.log('Service worker installing...');
-  self.skipWaiting();
+const cacheName = "v1";
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+    console.log("Service Workers: Installed");
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service worker activating...');
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((res) => {
+                //Make clone of response
+                const resClone = res.clone();
+                // Open cache
+                caches.open(cacheName).then((cache) => {
+                    // Add response to the cache
+                    cache.put(event.request, resClone);
+                });
+                return res;
+            })
+            .catch((err) =>
+                caches
+                    .match(event.request)
+                    .then((res) => res)
+                    .catch((err) => console.error(err))
+            )
+    );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Fetching:', event.request.url);
+// Update a service worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
