c18a1d9503059e094696769df658488fd8990a40
Millardiang <78812410+Millardiang@users.noreply.github.com>
2023-06-15 14:11:25 +0100

    Bumped tp alpha 0.4.23
    Page footer is now an include
    OS version added to footer

--- www/dvmIndexTablet.php
+++ www/dvmIndexTablet.php
@@ -163,29 +163,16 @@ error_reporting(0);
 
 <!--end outdoor data-->
 
-<!--footer area for homeweatherstation template warning dont mess with this below this line unless you really know what you are doing-->
-<div class="divumfooter-container">
-  <div class="divumfooter-item">
-    <div class="hardwarelogo1"><a href="http://weewx.com" alt="http://weewx.com" title="http://weewx.com">
-        <?php echo '<img src="img/icon-weewx.svg" alt="WeeWX" title="WeeWX" width="150px" height="55px"><div class="hardwarelogo1text"></div>';?></a>
-      </div>
+<!--End Main Grid area-->
 
-    <div class="hardwarelogo2">
-      <?php
-echo '<a href="https://https://claydonsweather.org.uk/" title="https://claydonsweather.org.uk/" target="_blank"><br><img src="img/divumLogo.svg" width="40px" alt="https://https://claydonsweather.org.uk/" class="homeweatherstationlogo" ><divumwx>Team DivumWX design in progress 2021-' . date('Y') . '</divumwx></a>';?>
-    </div>
+<!--footer area -->
+<?php
 
-    <div class="footertext">
-       <?php echo $info;?> (<value><?php echo $templateversion;?></value>) <?php echo "WeeWX";?>-(<value>
-        <maxred><?php echo $divum["swversion"];?>
-      </value>) <?php echo $info . " " . $weatherhardware;?></div>
-    <div class="footertext"><a href="https://github.com/steepleian/weewx-divumwx"><?php echo $github;?>  WeeWX Version Repository at https://github.com/steepleian/weewx-divumwx  <img src="img/flags/<?php echo $flag;?>.svg" width="20px"></a></div>
-    <div class="footertext">
-      <a href="https://www.aerisweather.com/"><img src="img/aerisweather-attribution-h-<?php echo $theme;?>.png" width="75px"></a></br><a href="https://developer.yr.no/featured-products/forecast/">    Meteogram Data by <img src="img/yr.svg" width="14px"></a></br><a href="https://bas.dev/work/meteocons">    Animated Icons by <img src="img/bm.svg" width="14px"></a>
-    </div>
-  </div>
-</div>
-<div class="menuadmin">
+include_once ('dvmFooter.php');
+
+?>
+
+<!--end of footer area --> <div class="menuadmin">
   
   <!-- Top Bar -->
   <header class="menuadmin__header">
