b8f33538393fb4ee8dff6d2b3b83cb65d33c96bf
Millardiang <78812410+Millardiang@users.noreply.github.com>
2023-10-30 22:24:42 +0000

    Bumped to 0.6.9

--- www/dvmVersion.php
+++ www/dvmVersion.php
@@ -11,5 +11,5 @@
 #                                                                                                                   #
 #####################################################################################################################
 
-$templateversion = "DVM-<maxblue>0.6.8</maxblue>alpha";
+$templateversion = "DVM-<maxblue>0.6.9</maxblue>alpha";
 ?>
