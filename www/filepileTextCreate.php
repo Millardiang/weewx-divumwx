2c42c6483eae6b09b42b837f01815d00d7c8f286
Millardiang <78812410+Millardiang@users.noreply.github.com>
2024-01-01 22:49:48 +0000

    Create filepileTextCreate.php

---
+++ www/filepileTextCreate.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Include a stanza like this in your weewx.conf file: -
+
+###############################################################################
+
+# Options for extension 'filepile'
+[FilePile]
+    # Where to find the incoming new data:
+    filename = /var/www/html/divumwx/serverdata/filepileTextData.txt
+    # What unit system they will be in.
+    # Choices are 'US', 'METRIC', or 'METRICWX'
+    unit_system = US
+    # Map from incoming names, to WeeWX names.
+    [[label_map]]
+        cloudCover = cloud_cover
+        pm25 = pm2_5
+        pm10 = pm10_0
+
+################################################################################
+
+You must also create a dummy file in your serverdata folder: -
+
+filepileTextData.txt with permissions set to 0777 to make it writeable and executable
+
+*/
+
+include_once ('dvmUpdater.php');
+$json_cloud = file_get_contents("/var/www/html/divumwx/jsondata/awc.txt");
+$cloud = json_decode($json_cloud, true);
+$cloudcover = $cloud['response'][0]['periods'][0]['sky'];
+
+$myfile = fopen("/var/www/html/divumwx/serverdata/filepileTextData.txt", "w") or die("Unable to open file!");
+
+$txt = "cloudCover = $cloudcover";
+fwrite($myfile, $txt);
+fclose($myfile);
+
+?>
