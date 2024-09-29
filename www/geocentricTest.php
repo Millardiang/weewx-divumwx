ea5dadb79b23d5ce0be5ad7dee66ec4e96bf662a
Millardiang <78812410+Millardiang@users.noreply.github.com>
2024-09-29 15:31:23 +0100

    Deleted redundant test code

--- www/geocentricTest.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include('dvmCombinedData.php');
-
-$sunScript = exec('python3 /var/www/html/divumwx/sun2.py $lat $lon $elevation'); $sunOutput = shell_exec($sunScript);
-$moonScript = exec('python3 /var/www/html/divumwx/moon2.py $lat $lon $elevation'); $moonOutput = shell_exec($moonScript);
-//echo $sunOutput."</br>";
-echo $moonOutput;?>
