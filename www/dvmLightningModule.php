dc496bc71c68a65f8080bee71978ec129ae01fef
Millardiang <78812410+Millardiang@users.noreply.github.com>
2025-01-23 21:40:03 +0000

    Update dvmLightningModule.php
    Corrected CSS issue on phone and tablet screens

--- www/dvmLightningModule.php
+++ www/dvmLightningModule.php
@@ -15,7 +15,6 @@
 ############################################################################################## 
 include('dvmCombinedData.php');
 date_default_timezone_set($TZ);
-
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +23,7 @@ date_default_timezone_set($TZ);
 <div class="chartforecast">
 <span class="yearpopup"><a alt="aquinfo" title="Lightning Almanac" href="dvmLightningRecords.php" data-lity><?php echo $menucharticonpage;?> Lightning Records | Radar | Maps</a></span>
 </div>
-<span class='moduletitle'><?php echo $lang['lightningModule'];?><?php if (filesize('jsondata/NSDRealtime.txt') < 100) { echo "&nbsp;" , $offline;} else echo "";?></span>
+<span class='moduletitle'>Recorded&nbsp;<?php echo $lang['lightningModule'];?>&nbsp;Strikes<?php if (filesize('jsondata/NSDRealtime.txt') < 100) { echo "&nbsp;" , $offline;} else echo "";?></span>
 
 <div class="updatedtime1"><span><?php if(file_exists($livedata)&&time() - filemtime($livedata)>300) echo $offline. '<offline> Offline </offline>'; else echo $online." ".$divum["time"];?></div>
 
@@ -109,27 +108,27 @@ if (empty($lightningBolt[22])) {
 $lightning["unixtimestamp"]           	= $lightningBolt[0]; // unix timestamp
 $lightning["rate_per_min"]            	= $lightningBolt[1]; // current rate/min
 $lightning["close_rate_per_min"]      	= $lightningBolt[2]; // current close rate/min (< 50km)
-$lightning['last_time']      			= $lightningBolt[3]; // last strike date and time
-$lightning["bearing"] 					= $lightningBolt[4]; // Bearing number
-$lightning["bearingx"] 					= $lightningBolt[4]; // Bearing ordinals 
-$lightning["last_distance"]  			= $lightningBolt[5]; // last strike distance 
-$lightning["last_strike_type"]      	= $lightningBolt[6]; // last strike type ( CC+, CC-, CG+, CG- )
-$lightning["hour_strike_count"]     	= $lightningBolt[7]; // strikes last hour
-$lightning["today_strike_count"]  		= $lightningBolt[8]; // strikes today
-$lightning["month_strike_count"]   		= $lightningBolt[9]; // strikes this month
-$lightning["year_strike_count"]    		= $lightningBolt[10]; // strikes this year
-$lightning["max_rate_per_min"]			= $lightningBolt[11]; // max rate/min
-$lightning["max_ratetime"]				= $lightningBolt[12]; // max ratetime (hh:mm)
-$lightning["max_burst"]					= $lightningBolt[13]; // max burst/s
-$lightning["max_burst_time"]			= $lightningBolt[14]; // max bursttime (hh:mm)
-$lightning["CG+_strikes"]				= $lightningBolt[15]; // CG+ strikes today
-$lightning["CG-_strikes"]				= $lightningBolt[16]; // CG- strikes today
-$lightning["CC+_strikes"]				= $lightningBolt[17]; // CC+ strikes today
-$lightning["CC-_strikes"]				= $lightningBolt[18]; // CC- strikes today
-$lightning["uptime"]					= $lightningBolt[19]; // uptime (x days x hours x mins)
-$lightning["unitsx"]					= $lightningBolt[20]; // km or miles (set in config.ini)
-$lightning["persistence"]				= $lightningBolt[21]; // Persistence in minutes set 60 mins
-$lightning["nsdcrop"]					= $lightningBolt[22]; // Max strikes in NSDStrikes file (set to 2000)
+$lightning['last_time']      						= $lightningBolt[3]; // last strike date and time
+$lightning["bearing"] 									= $lightningBolt[4]; // Bearing number
+$lightning["bearingx"] 									= $lightningBolt[4]; // Bearing ordinals 
+$lightning["last_distance"]  						= $lightningBolt[5]; // last strike distance 
+$lightning["last_strike_type"]      		= $lightningBolt[6]; // last strike type ( CC+, CC-, CG+, CG- )
+$lightning["hour_strike_count"]     		= $lightningBolt[7]; // strikes last hour
+$lightning["today_strike_count"]  			= $lightningBolt[8]; // strikes today
+$lightning["month_strike_count"]   			= $lightningBolt[9]; // strikes this month
+$lightning["year_strike_count"]    			= $lightningBolt[10]; // strikes this year
+$lightning["max_rate_per_min"]					= $lightningBolt[11]; // max rate/min
+$lightning["max_ratetime"]							= $lightningBolt[12]; // max ratetime (hh:mm)
+$lightning["max_burst"]									= $lightningBolt[13]; // max burst/s
+$lightning["max_burst_time"]						= $lightningBolt[14]; // max bursttime (hh:mm)
+$lightning["CG+_strikes"]								= $lightningBolt[15]; // CG+ strikes today
+$lightning["CG-_strikes"]								= $lightningBolt[16]; // CG- strikes today
+$lightning["CC+_strikes"]								= $lightningBolt[17]; // CC+ strikes today
+$lightning["CC-_strikes"]								= $lightningBolt[18]; // CC- strikes today
+$lightning["uptime"]										= $lightningBolt[19]; // uptime (x days x hours x mins)
+$lightning["unitsx"]										= $lightningBolt[20]; // km or miles (set in config.ini)
+$lightning["persistence"]								= $lightningBolt[21]; // Persistence in minutes set 60 mins
+$lightning["nsdcrop"]										= $lightningBolt[22]; // Max strikes in NSDStrikes file (set to 2000)
 
 
 		// Bearing
@@ -167,18 +166,27 @@ $lightning["nsdcrop"]					= $lightningBolt[22]; // Max strikes in NSDStrikes fil
 			$lightning["bearingx"]='NNW';
 		}else {$lightning["bearingx"]='North';}
 	} 
+if ($wind["units"] == "mph"){$lightning["last_distance"] = $lightning["last_distance"] * 0.621371; $lightning["distunit"] = "miles";} else {$lightning["distunit"] = "km";}?>
 
-if ($wind["units"] == "mph"){$lightning["last_distance"] = $lightning["last_distance"] * 0.621371; $lightning["distunit"] = "miles";} else {$lightning["distunit"] = "km";}
-?>
-<style>
-table.lightning {font-family: Arial, Helvetica, sans-serif;border:0px solid transparent;border-radius:2px;border-spacing: 3px;background-color: transparent;width: 75%;height:70%;text-align: center;}
-table.lightning td, table.lightning th {border: 1px solid #000000;padding: 1px 1px;border:1px solid var(--col-13);border-radius:2px;}
-table.lightning tbody td {font-size: 0.6em;color: var(--col-6);}
-table.lightning thead th {font-size: 12px;font-weight: normal;text-align: center;padding: 0px 0px;}
-</style>
 <script src="js/d3.7.9.0.min.js"></script>
-<div class="lightning-module" style="display:grid;grid-template-columns: auto auto;">
-<div class="lightning-img">
+<style>
+table.lightning {
+  border-spacing: 0.25em;
+  color: var(--col-6);
+  text-align: center;
+}
+table.lightning td, table.lightning th {
+  padding: 1px 2px;
+  border: 1px solid var(--col-13);
+  border-radius:2px;
+  border-left: 5px solid #7DF9FF;
+}
+table.lightning tbody td {
+  font-size: 10px;
+}
+</style>
+<div class="lightning-grid" style="display:grid;grid-template-columns: auto auto;">
+<div class="image-container">
 <div class="Strikes"></div>
 <div class="base">
 <svg id="base" width="50" height="100" viewBox="0 0 88.7 90">
@@ -188,51 +196,44 @@ table.lightning thead th {font-size: 12px;font-weight: normal;text-align: center
 </svg>
 </div>
 </div>
-
-<table class="lightning" style="margin-left:-166px;margin-top:0px;">
+<div class="table-container">
+<table class="lightning" style="margin-left:-180px;margin-top:-1px;">
 <tbody>
-<thead>
 <tr>
-<th colspan="2"style="border:transparent;">Recorded Strikes</th>
-</tr>
-</thead>
-<tr>
-<td style="border:transparent;">Detected</td>
+<td style="border:transparent;">Last Strike</td>
 <td style="border:transparent;">Distance</td>
 </tr>
 <tr>
-<td style="border-left: 5px solid orange;width:30%;"><?php echo date('d m H:i',$lightning["last_time"]);?></td>
-</td>
-<td style="border-left: 5px solid rgb(121,164,201);width:30%;"><?php echo number_format($lightning["last_distance"],1);?></td>
+<td><?php echo date('jS M H:i',$lightning["last_time"]);?></td>
+<td><?php echo number_format($lightning["last_distance"],1);?></td>
 </tr>
 <tr>
 <td style="border:transparent;">Last Hour</td>
 <td style="border:transparent;">Month</td>
 </tr>
 <tr>
-<td style="border-left: 5px solid rgb(74,137,92);width:30%;"><?php echo $lightning["hour_strike_count"];?></td>
-<td style="border-left: 5px solid orange;width:30%;"><?php echo $lightning["month_strike_count"];?></td>
+<td><?php echo $lightning["hour_strike_count"];?></td>
+<td><?php echo $lightning["month_strike_count"];?></td>
 </tr>
 <tr>
 <td style="border:transparent;">Year</td>
 <td style="border:transparent;">Alltime</td>
 </tr>
 <tr>
-<td style="border-left: 5px solid rgb(121,164,201);width:30%;"><?php echo $lightning["year_strike_count"];?></td>
-<td style="border-left: 5px solid rgb(74,137,92);width:30%;"><?php echo $lightning["alltime_strike_count"] + $lightning["year_strike_count"];?></td>
+<td><?php echo $lightning["year_strike_count"];?></td>
+<td><?php echo $lightning["alltime_strike_count"];?></td>
 </tr>
-<?php if ($lightningSource==0){echo
-'<tr>
+<?php
+if ($lightningSource == 0)
+{echo '<tr>
 <td style="border:transparent;">Bearing</td>
-<td style="border-left: 5px solid rgb(74,137,92);width:30%;">'.$lightning["bearingx"].'&nbsp;'.$lightning["bearing"].'</td></tr>
-<tr>
-</tr>';
-}?>
-
+<td><?php echo $lightning["bearingx"];?>&nbsp;<?php echo $lightning["bearing"];?></td>
+</tr>';}
+ ?>
 </tbody>
 </table>
 </div>
-</html>
+</div>
 
 <script>
 	var svg = d3.select(".Strikes")
@@ -257,7 +258,7 @@ table.lightning thead th {font-size: 12px;font-weight: normal;text-align: center
 					.transition()
 					.duration(250)
 					.attr('fill', "none")
-					.attr('stroke', '#6CA6CD');
+					.attr('stroke', '#7DF9FF');
 
 		svg.append('polyline') // Lightning bolt
 					.attr("cx", 40)
@@ -266,7 +267,7 @@ table.lightning thead th {font-size: 12px;font-weight: normal;text-align: center
 					.transition()
 					.duration(250)
 					.attr('fill', "none")
-					.attr('stroke', '#6CA6CD');
+					.attr('stroke', '#7DF9FF');
 
 		svg.append('polyline') // Lightning bolt
 					.attr("cx", 40)
@@ -275,9 +276,8 @@ table.lightning thead th {font-size: 12px;font-weight: normal;text-align: center
 					.transition()
 					.duration(250)
 					.attr('fill', "none")
-					.attr('stroke', '#6CA6CD');
+					.attr('stroke', '#7DF9FF');
+
+
 </script>
-
-
-
-
+</html>
