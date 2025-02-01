4dbeee552bda53ca8f8cf8961fb717d6aa820fef
Millardiang <78812410+Millardiang@users.noreply.github.com>
2025-02-01 12:36:25 +0000

    Adding back admin folder and parts. New design dvmCurrentConditions.php

---
+++ www/chartVars.php
@@ -0,0 +1,5 @@
+<?php
+$baromWeek = "baromCharts.php?chart=barometerplot&span=weekly&temp=" . urlencode($temp["units"]) . "&pressure=" . urlencode($barom["units"]) . "&wind=" . urlencode($wind["units"]) . "&rain=" . urlencode($rain["units"]);
+$baromYear = "baromCharts.php?chart=barometerplot&span=yearly&temp=" . urlencode($temp["units"]) . "&pressure=" . urlencode($barom["units"]) . "&wind=" . urlencode($wind["units"]) . "&rain=" . urlencode($rain["units"]);
+?>
+
